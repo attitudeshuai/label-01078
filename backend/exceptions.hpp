788c72eb9d98aeefb0db0d198bf8bda6d374d42d
李丽仙 <lilixian@benzhi.io>
2026-05-08 02:17:15 +0800

    TestM_2

---
+++ backend/exceptions.hpp
@@ -0,0 +1,17 @@
+#ifndef EXCEPTIONS_HPP
+#define EXCEPTIONS_HPP
+
+#include <stdexcept>
+#include <string>
+
+class ValidationException : public std::runtime_error {
+public:
+    explicit ValidationException(const std::string& msg) : std::runtime_error(msg) {}
+};
+
+class BusinessException : public std::runtime_error {
+public:
+    explicit BusinessException(const std::string& msg) : std::runtime_error(msg) {}
+};
+
+#endif // EXCEPTIONS_HPP
